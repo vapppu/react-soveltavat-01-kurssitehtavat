0b79d4fc1e0efddeb70d5f55351eb90bcfca57f7
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-29 13:05:03 +0200

    Fix typos in configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-luku2-oppimistehtava-kurssitiedot/",
+  base: "/react-soveltavat-01-kurssitehtavat/",
   plugins: [react()],
 })
