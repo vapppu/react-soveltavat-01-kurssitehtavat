7dc05a6e9a504d01e4d3e3a6109af17d70e91201
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-29 13:01:17 +0200

    Prepare project for publishing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-luku2-oppimistehtava-kurssitiedot/",
   plugins: [react()],
 })
